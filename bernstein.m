b1dcaa787d55ee9628d464671dc5ae5ada9b00e5
Emily Justus <emilyjustus826@gmail.com>
2018-12-12 14:18:20 -0600

    Update bernstein.m

--- bernstein.m
+++ bernstein.m
@@ -1,10 +1,13 @@
 
+%Set of points that will form a heart, but will be two different colors thus being two graphs in one
 P = [0 1 1 1.5 2 0.5 1.5 -1 0 -1; 0 -1 -1.5 -1 -2 0.5 -1 1.5 0 1];
 clf;  %Clear figures
 
+%iterate each half of the heart
 for m=1:2
-%Plot control points
+    %iterate of each set of points in both halves
     for k=1:5
+    %Plot the control points
      plot(P(m,2*k-1),P(m,2*k),'r.','MarkerSize',20);
      hold on;
     end
@@ -20,7 +23,7 @@ for m=1:2
     B3 = B(3,4,t);
     B4 = B(4,4,t);
 
-    % Summing them together.
+    % Multiplying the individual value of each point to the Berstein function then summing them together
     x= P(m,1) * B0  +  P(m,3) * B1  +  P(m,5) * B2  +  P(m,7) * B3 + P(m,9) * B4;
     y= P(m,2) * B0  +  P(m,4) * B1  +  P(m,6) * B2  +  P(m,8) * B3 + P(m,10) * B4;
 
