30b711ac5fdcb3958d1cb6fcf2bd17545fddde10
Emily Justus <emilyjustus826@gmail.com>
2018-12-12 14:20:57 -0600

    Update bens_1.m

--- bens_1.m
+++ bens_1.m
@@ -1,4 +1,7 @@
 
+%File similar to "bernstein.m" but only makes one graph out of the 10 points
+
+%P = control points to form a heart
 P = [0 1 1 1.5 2 0.5 1.5 -1 0 -1 0 -1 -1.5 -1 -2 0.5 -1 1.5 0 1];
 clf;  %Clear figures
 
@@ -24,7 +27,7 @@ B7 = B(7,9,t);
 B8 = B(8,9,t);
 B9 = B(9,9,t);
 
-% Summing them together.
+% Multiplying the values of each point to their respective Bernstein function then summing them together
 x= P(1,1) * B0  +  P(1,3) * B1  +  P(1,5) * B2  +  P(1,7) * B3 + P(1,9) * B4 + P(1,11) * B5 + P(1,13) * B6 + P(1,15) * B7 + P(1,17) * B8 + P(1,19) * B9;
 y= P(1,2) * B0  +  P(1,4) * B1  +  P(1,6) * B2  +  P(1,8) * B3 + P(1,10) * B4 + P(1,12) * B5 + P(1,14) * B6 + P(1,16) * B7 + P(1,18) * B8 + P(1,20) * B9;
 
